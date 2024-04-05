f2cf07ba3e4ef0e59b1c75a89ffd521dc550a034
jonanoj <najhy.nan@gmail.com>
2024-04-05 03:06:09 +0300

    CustomKeybinds: Add advanced settings menu & keybind_button

--- CustomKeybinds/reframework/autorun/Keybinds.lua
+++ CustomKeybinds/reframework/autorun/Keybinds.lua
@@ -1,3 +1,10 @@
+--[[
+Custom Keybinds by jondnoj
+- https://www.nexusmods.com/dragonsdogma2/mods/297/
+- https://github.com/jonanoj/dd2-mods/tree/main/CustomKeybinds
+ 
+Thanks to alphaZomega for the base code for keybind_button
+]] --
 local function generate_enum(typename)
     local t = sdk.find_type_definition(typename)
     if not t then
@@ -30,6 +37,96 @@ local function sdk_iterator(sdk_enumerator)
         end
     end
 end
+
+local keyboard_name_to_id, keyboard_id_to_name = generate_enum("via.hid.KeyboardKey")
+local mouse_name_to_id, mouse_id_to_name = generate_enum("via.hid.MouseButton")
+
+local recording_button_id = nil
+local last_pressed_key = nil
+
+local function _keybind_button(id, current_value, width)
+    local button_size = {}
+    table.insert(button_size, 120)
+    table.insert(button_size, 30)
+
+    if recording_button_id == id then
+        if last_pressed_key then
+            recording_button_id = nil
+
+            -- Treat Escape as 'Reset keybind'
+            if last_pressed_key == keyboard_name_to_id.Escape then
+                return true, 0
+            end
+
+            return true, last_pressed_key
+        end
+
+        if imgui.button("[Press any key]", button_size) then
+            recording_button_id = nil
+            return false, nil
+        end
+
+        if imgui.is_item_hovered() then
+            imgui.set_tooltip(
+                "To bind mouse keys, Press anywhere *OUTSIDE* the REFramework UI. \nPress Escape to remove keybind.")
+        else
+
+        end
+    else
+        if imgui.button(keyboard_id_to_name[current_value], button_size) then
+            recording_button_id = id
+        end
+    end
+
+    return false, nil
+end
+
+local function keybind_button(id, current_value, width)
+    imgui.push_id(id)
+    local changed, value = _keybind_button(id, current_value, width)
+    imgui.pop_id()
+    return changed, value
+end
+
+local mouse_to_keyboard_key = {
+    [mouse_name_to_id.L] = keyboard_name_to_id.LButton,
+    [mouse_name_to_id.R] = keyboard_name_to_id.RButton,
+    [mouse_name_to_id.C] = keyboard_name_to_id.MButton,
+    [mouse_name_to_id.EX0] = keyboard_name_to_id.XButton1,
+    [mouse_name_to_id.EX1] = keyboard_name_to_id.XButton2
+}
+
+local keyboard = sdk.get_native_singleton("via.hid.Keyboard")
+local mouse = sdk.get_native_singleton("via.hid.Mouse")
+local keyboard_type = sdk.find_type_definition("via.hid.Keyboard")
+local mouse_type = sdk.find_type_definition("via.hid.Mouse")
+
+re.on_application_entry("UpdateHID", function()
+    if not recording_button_id then
+        last_pressed_key = nil
+        return nil
+    end
+
+    local mouse_device = sdk.call_native_func(mouse, mouse_type, "get_Device")
+    if mouse_device then
+        for id, keyboard_key in pairs(mouse_to_keyboard_key) do
+            if mouse_device:call("isRelease", id) then
+                last_pressed_key = keyboard_key
+                return
+            end
+        end
+    end
+
+    local keyboard_device = sdk.call_native_func(keyboard, keyboard_type, "get_Device")
+    if keyboard_device then
+        for id, name in pairs(keyboard_id_to_name) do
+            if keyboard_device:call("isDown", id) then
+                last_pressed_key = id
+                return
+            end
+        end
+    end
+end)
 
 local gamepad_name_to_id, gamepad_id_to_name = generate_enum("via.hid.GamePadButton")
 local supported_gamepad_buttons = {}
@@ -64,7 +161,6 @@ end, function(retval)
     return retval
 end)
 
-local keyboard_name_to_id, keyboard_id_to_name = generate_enum("via.hid.KeyboardKey")
 local character_action_name_to_id, character_action_id_to_name = generate_enum("app.CharacterInput.Action")
 local system_action_name_to_id, system_action_id_to_name = generate_enum("app.SystemInput.Action")
 local get_message = sdk.find_type_definition("via.gui.message"):get_method("get")
@@ -177,11 +273,30 @@ local function get_key_name(keybind)
     return "(UNKNOWN ACTION)"
 end
 
-local function render_keybind(keybind)
+local function render_keybind(keybind, show_keys)
     imgui.text(get_key_name(keybind))
 
     imgui.indent()
+
     local with_shift = keybind:call("get_WithCustomShift")
+    local command_hash = keybind:call("get_CommandHash")
+
+    if show_keys then
+        local key1Changed, key1Value = keybind_button(command_hash .. "-1", keybind:call("get_PrimaryKey"))
+        if key1Changed then
+            keybind:set_field("_PrimaryKey", key1Value)
+            return true, with_shift
+        end
+
+        imgui.same_line()
+
+        local key1Changed, key1Value = keybind_button(command_hash .. "-2", keybind:call("get_SecondaryKey"))
+        if key1Changed then
+            keybind:set_field("_SecondaryKey", key1Value)
+            return true, with_shift
+        end
+    end
+
     local shift_changed, should_disable = imgui.checkbox("Disable 'Switch Weapon Skill'?", not with_shift) -- Notice the negation here, UI feels more intuitive when patching is "V"
     if shift_changed then
         local with_shift = not should_disable -- Double negative, we treat this value as "WithCustomShift" internally
@@ -193,19 +308,52 @@ local function render_keybind(keybind)
     return false, nil
 end
 
+local function save_shift_change(keybind, command_hash, with_shift)
+    update_keybind_state(keybind, with_shift)
+
+    settings.patched_commands[command_hash] = with_shift
+    json.dump_file(settings_file, settings)
+end
+
 re.on_draw_ui(function()
     if imgui.tree_node("Custom Keybinds") then
+
+        if imgui.tree_node("Advanced Settings") then
+            imgui.spacing()
+            imgui.spacing()
+            imgui.spacing()
+            imgui.begin_rect()
+            imgui.text("Use this menu to bind keys that can't be changed through the in-game UI")
+            imgui.spacing()
+            imgui.spacing()
+
+            for command_hash in sdk_iterator(KeyBindTable:call("get_Keys")) do
+                local keybind = KeyBindTable:call("get_Item", command_hash)
+                imgui.push_id("advanced-keyboard-key-" .. command_hash)
+                local changed, with_shift = render_keybind(keybind, true)
+                if changed then
+                    save_shift_change(keybind, command_hash, with_shift)
+                end
+                imgui.spacing()
+                imgui.spacing()
+                imgui.pop_id()
+            end
+
+            imgui.end_rect(10, true)
+            imgui.spacing()
+            imgui.spacing()
+            imgui.spacing()
+            imgui.spacing()
+        end
+
         imgui.text("Force disable 'Ctrl+X' keys:")
         imgui.spacing()
         for i, command_hash in pairs(command_hashes_with_shift) do
             imgui.push_id("keyboard-key-" .. command_hash)
             local keybind = KeyBindTable:call("get_Item", command_hash)
-            local changed, with_shift = render_keybind(keybind)
+            local changed, with_shift = render_keybind(keybind, false)
             if changed then
-                update_keybind_state(keybind, with_shift)
-
-                settings.patched_commands[command_hash] = with_shift
-                json.dump_file(settings_file, settings)
+                save_shift_change(keybind, command_hash, with_shift)
             end
             imgui.spacing()
             imgui.spacing()
